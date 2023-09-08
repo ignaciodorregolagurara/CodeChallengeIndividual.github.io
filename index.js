307b4d098859ed66208c10578a1aa44017994c19
ignaciodorrego <ignadorrego@gmail.com>
2023-09-08 20:34:14 -0300

    Cambios Finalizados

--- index.js
+++ index.js
@@ -1,66 +1,63 @@
-let nombrePersona = document.getElementById("nom");
-let apellidoPersona = document.getElementById("ape");
-let fechaNacimientoPersona = document.getElementById("nac");
-/* */
 let btnEnvio = document.getElementById("btnEnvio");
 let btnLimpiar = document.getElementById("btnLimpiar");
 /* */
-let infoFormulario = document.getElementById("infoDeFormulario");
-/* */
-let URLJson = 'https://jsonplaceholder.typicode.com/users';
+let URLJson = `https://jsonplaceholder.typicode.com/users`;
 
-btnLimpiar.addEventListener("click", function(e) {
-    e.preventDefault();
-    nombrePersona.value = "";
-    apellidoPersona.value = "";
-    fechaNacimientoPersona.value = "";
-})
-
-btnEnvio.addEventListener("click", function(e) {
-    e.preventDefault();
-    nom = nombrePersona.value;
-    ape = apellidoPersona.value;
-    nac = fechaNacimientoPersona.value;
+document.addEventListener("DOMContentLoaded", function() {
+    btnLimpiar.addEventListener("click", function(e) {
+        e.preventDefault();
+        document.getElementById("nom").value = "";
+        document.getElementById("ape").value = "";
+        document.getElementById("nac").value = "";
+    })
     
-    let infoPersona = {
-        nombre: nom,
-        apellido: ape,
-        fechaNacimiento: nac
-    };
-    /* console.log(infoPersona); */
-
-    fetch(URLJson, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(infoPersona)
-    })
-    .then(function(response) {
-        return response.json()
-    })
-    .then(function(data) {
-        if (data.nombre != "" && data.apellido != "" && data.fechaNacimiento != "") {
-            console.log(`Esta es la respuesta que nos envia el servidor: `, data)
-            let AuxInfo = document.createElement("p");
-            AuxInfo.innerHTML += `
-                <strong>Nombre de la Persona:</strong> ${data.nombre}
-                <br>
-                <strong>Apellido de la Persona:</strong> ${data.apellido}
-                <br>
-                <strong>Fecha de Nacimiento de la Persona:</strong> ${data.fechaNacimiento}
-            `;
-            infoFormulario.appendChild(AuxInfo);
-            infoFormulario.classList.add("borderAux")
-        } else {
-            console.log("Faltan datos");
-            alert("Faltan datos");
-        }
-        /* */
-    })
-    .catch(function(error) {
-        console.log(`Se ha encontrado un error del siguiente tipo: `,error);
-    })
-
+    btnEnvio.addEventListener("click", function(e) {
+        e.preventDefault();
+        let nom = document.getElementById("nom").value;
+        let ape = document.getElementById("ape").value;
+        let nac = document.getElementById("nac").value;
         
+        let infoPersona = {
+            nombre: nom,
+            apellido: ape,
+            fechaNacimiento: nac
+        };
+        /*console.log(infoPersona);*/
+    
+        fetch(URLJson, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(infoPersona)
+        })
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+                if (nom != "" && ape != "" && nac != "") {
+                    console.log(`Esta es la respuesta que nos envia el servidor: `, data);
+                    let infoFormulario = document.getElementById("infoDeFormulario");
+                    let AuxInfo = document.createElement("p");
+                    AuxInfo.innerHTML += `
+                        <strong>Nombre de la Persona:</strong> ${data.name}
+                        <br>
+                        <strong>Apellido de la Persona:</strong> ${data.apellido}
+                        <br>
+                        <strong>Fecha de Nacimiento de la Persona:</strong> ${data.fechaNacimiento}
+                    `;
+                    infoFormulario.appendChild(AuxInfo);
+                    infoFormulario.classList.add("borderAux");
+                } else {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Algo salió mal!',
+                        text: 'Alguno de los campos esta vacio',
+                    });
+                }
+        })
+        .catch(function(error) {
+            console.log(`Se ha encontrado un error del siguiente tipo: `, error);
+        })
+    })
 })
