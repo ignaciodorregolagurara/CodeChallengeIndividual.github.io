30c1237c9810637a44c86d053a5ae90f0a001ec3
ignaciodorrego <ignadorrego@gmail.com>
2023-09-08 18:48:06 -0300

    Finalizando funcionalidad del formulario

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 let nombrePersona = document.getElementById("nom");
 let apellidoPersona = document.getElementById("ape");
 let fechaNacimientoPersona = document.getElementById("nac");
+/* */
 let btnEnvio = document.getElementById("btnEnvio");
 let btnLimpiar = document.getElementById("btnLimpiar");
+/* */
+let infoFormulario = document.getElementById("infoDeFormulario");
+/* */
 let URLJson = 'https://jsonplaceholder.typicode.com/users';
 
 btnLimpiar.addEventListener("click", function(e) {
@@ -25,11 +29,38 @@ btnEnvio.addEventListener("click", function(e) {
     };
     /* console.log(infoPersona); */
 
-    fetch(URLJson)
-        .then(function(response) {
-            response.json()
-        })
-        .then(function() {
-            
-        })
+    fetch(URLJson, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(infoPersona)
+    })
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(data) {
+        if (data.nombre != "" && data.apellido != "" && data.fechaNacimiento != "") {
+            console.log(`Esta es la respuesta que nos envia el servidor: `, data)
+            let AuxInfo = document.createElement("p");
+            AuxInfo.innerHTML += `
+                <strong>Nombre de la Persona:</strong> ${data.nombre}
+                <br>
+                <strong>Apellido de la Persona:</strong> ${data.apellido}
+                <br>
+                <strong>Fecha de Nacimiento de la Persona:</strong> ${data.fechaNacimiento}
+            `;
+            infoFormulario.appendChild(AuxInfo);
+            infoFormulario.classList.add("borderAux")
+        } else {
+            console.log("Faltan datos");
+            alert("Faltan datos");
+        }
+        /* */
+    })
+    .catch(function(error) {
+        console.log(`Se ha encontrado un error del siguiente tipo: `,error);
+    })
+
+        
 })
